500fe74fe538d8ec6589d29290b8d8f372d56df6
Rick <101254647+nauish@users.noreply.github.com>
2023-07-23 19:11:31 +0800

    Solve pset 01-05

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,6 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    if (times > -1 )return `${string}`.repeat(times);
+    else return 'ERROR'
 };
 
 // Do not edit below this line
