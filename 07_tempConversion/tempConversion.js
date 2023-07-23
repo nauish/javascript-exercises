388298fcd7c9e14f9aadaab16e96bad93f12ae98
Rick <101254647+nauish@users.noreply.github.com>
2023-07-23 19:40:56 +0800

    Solve pset 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  if (Number.isFinite(f)){
+    const c = (f - 32 ) * 5/9 
+    if (Number.isInteger(c)) {
+      return c
+    } return Math.round(c*10)/10
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  if (Number.isFinite(c)){
+    const f = c * 9 / 5 + 32 
+    if (Number.isInteger(f)) {
+      return f
+    } return Math.round(f*10)/10
+  }
 };
 
 // Do not edit below this line
