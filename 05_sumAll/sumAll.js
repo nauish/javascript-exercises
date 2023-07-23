fb73c0c7070909d921b905e82585f53b92f9d9fa
Rick <101254647+nauish@users.noreply.github.com>
2023-07-23 19:10:44 +0800

    Solve pset 01-05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0
+    if (start < 0 || end < 0 || Number.isInteger(start) != 1 || Number.isInteger(end) != 1) {
+        return 'ERROR'
+    } else if (start < end) {
+        for (let i = start; i <= end; i++) {
+            sum += i
+        }
+    } else {
+        for (let i = end; i <= start; i++) {
+            sum += i
+        }
+    }
+    return sum 
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
