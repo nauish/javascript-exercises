dcc2eeb084baddd938163dd333af9cbd06539918
Rick <101254647+nauish@users.noreply.github.com>
2023-09-03 22:11:56 +0800

    Add solutions

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (str = "racecar") {
+  const reducedStr = str.toLocaleLowerCase().replace(/[^a-z0-9]/g, "");
+  let left = 0;
+  let right = reducedStr.length - 1;
+  while (left < right) {
+    if (reducedStr[left] !== reducedStr[right]) return false;
+    left += 1;
+    right -= 1;
+  }
+  return true;
 };
+
+console.log(palindromes("Racecar!"));
 
 // Do not edit below this line
 module.exports = palindromes;
