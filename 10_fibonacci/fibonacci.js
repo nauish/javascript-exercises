9fce311b298f3fd9e5366446cc9858074653b73f
Rick <101254647+nauish@users.noreply.github.com>
2023-09-03 22:29:12 +0800

    Solve this recursively

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function (max) {
+  if (max <= 1) {
+    return max;
+  } else {
+    return fibonacci(max - 1) + fibonacci(max - 2);
+  }
 };
+console.log(fibonacci(4));
 
 // Do not edit below this line
 module.exports = fibonacci;
